e4be8df50ed2ba3c92e9084cf1c9572a423f9468
Felix Castro <felixcastro1993@gmail.com>
2021-02-18 17:52:01 -0500

    combinar listas

---
+++ almRepro.py
@@ -0,0 +1,5 @@
+#Elaborar un algoritmo con dos arreglos
+#el primero contiene m estudiantes que perdieron la materia de computadoras
+#el primero contiene n estudiantes que perdieron la materia de algoritmos
+#formas un tercer arreglo con los estudiantes que perdieron ambas materias
+
