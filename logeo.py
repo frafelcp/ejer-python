f7101c8c5d5cd2b599661738db764a8a0ee1bd89
Felix Castro <felixcastro1993@gmail.com>
2021-03-03 21:28:13 -0500

    refac menus, registra y comentar codigo

--- logeo.py
+++ logeo.py
@@ -1,16 +1,13 @@
-def registrarCuentas(cuentas):
+def registrar(cuentas):
     #leemos usuario
     user = input("Usuario -x para cortar:")
-    while user != "x":
-        #leemos contraseña
-        passw = input("Ingrese contraseña :")
-        #guardamos usuarios y sus contraseñas
-        cuentas[0].append(user)
-        cuentas[1].append(passw)
-        #leemos usuario
-        #user = input("Usuario -x para cortar :")
-        #retornamos usuarios y contraseñas en una lista de listas
-        return cuentas
+    #leemos contraseña
+    passw = input("Ingrese contraseña :")
+    #guardamos usuarios y sus contraseñas
+    cuentas[0].append(user)
+    cuentas[1].append(passw)
+    #retornamos usuarios y contraseñas en una lista de listas
+    return cuentas
     
         
 
@@ -18,30 +15,47 @@ def imprimirCuentas(cuentas):
     for row in cuentas:
         for elem in row:
             print(elem, end=' ')
-        print()
+        print() 
 
 
 #programa principal
+#lista bidimimensional que almacenara los usuarios y contraseñas
 cuentas = [[], []]
 
 while True:
-    #menu
-    print("1. Insertar usuarios")
-    print("2. Usuarios registrados")
-    print("3. Iniciar sesion")
-    print("4. Salir del programa")
+    #menu principal
+    print("****************************")
+    print("MENU PRINCIPAL")
+    print("1. REGISTRARSE")
+    print("2. INICIAR SESION")
+    print("3. CERRAR")
 
+    #escogemos una opcion del menu principal
     opcion = int(input("Accion a ejecutar: "))
 
     if opcion == 1:
-        print("INSERTAR NUEVO USUARIOS")
-        cuentas = registrarCuentas(cuentas)
+        #menu registro de usuarios
+        print("++++++++++++++++++++++++++++++")
+        print("MENU REGISTRO DE USUARIO")
+        while True:
+            print("REGISTRO")
+            print("1. REGISTRAR USUARIO")
+            print("2. REGRESAR")
+
+            #escogemos una opcion del menu principal registrar
+            opcionRegistrar = int(input("Accion a ejecutar: "))
+
+            if opcionRegistrar == 1:
+                cuentas = registrar(cuentas)
+            elif opcionRegistrar == 2:
+                print("REGRESAR A MENU PRINCIPAL")
+                break
+            else:
+                print("Opcion invalida")
     elif opcion == 2:
-        print("USUARIOS REGISTRADOS")
+        print("INICIAR SESION")
         cuentas = imprimirCuentas(cuentas)
     elif opcion == 3:
-        print("INICIAR SESION")
-    elif opcion == 4:
         print("SALIR DEL PROGRAMA")
         break
     else:
