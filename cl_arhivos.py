53051e32f098e4a7e13478b3eaedfffce8eaa8d1
Felix Castro <felixcastro1993@gmail.com>
2021-03-10 21:32:26 -0500

    Add, print and read in mod

--- cl_arhivos.py
+++ cl_arhivos.py
@@ -1,13 +1,29 @@
-#leemos usuario
-archivo = open("archivo.txt", "w") #metod para abrir el archivo
-#escribimos en el archivo
-frase = "Felix Castro estudiante de cuarto cuatrimestre de Tecnico Profesional en Sistemas Informatico"
-archivo.write(frase) #metodo para escribir en el archivo
-#cerramos la edicion del archivo
-archivo.close() #metodo para cerrar archivo
+def abrirEscribir():    
+    #leemos usuario
+    archivo = open("archivo.txt", "w") #metod para abrir el archivo
+    #escribimos en el archivo
+    frase = "Felix Castro estudiante de cuarto cuatrimestre de Tecnico Profesional en Sistemas Informatico"
+    archivo.write(frase) #metodo para escribir en el archivo
+    #cerramos la edicion del archivo
+    archivo.close() #metodo para cerrar archivo
 
-#abrir y leer archivo linea por linea
-archivoF = open("archivo.txt", "r")
-lineas = archivoF.readlines()
-archivoF.close()
-print(lineas)
+def abrirLeer():
+    #abrir y leer archivo linea por linea
+    archivoF = open("archivo.txt", "r")
+    lineas = archivoF.readlines()
+    archivoF.close()
+    print(lineas)
+
+def addArchivo():
+    archivoAdd = open("archivo.txt", "a")
+    archivoAdd.write("\nestas es otra linea")
+    leer = abrirLeer()
+
+
+print("-------------------------------")
+escribir = abrirEscribir()
+print("-------------------------------")
+read = abrirLeer()
+print("-------------------------------")
+agregar = addArchivo()
+print("-------------------------------")
